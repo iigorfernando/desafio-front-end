d807f5a6275c64e7c66c8fe3dc1cdb3df510867f
Igor Fernando <iigorfernando@gmail.com>
2018-03-24 05:39:27 -0300

    IF
    index.js Commit

--- index.js
+++ index.js
@@ -1,7 +1,45 @@
+'use strict';
+
 var express = require('express');
+var request = require('request');
+var hbs = require('express-hbs');
+var path = require('path');
+
 var app = express();
 
-app.use('/', express.static('htdocs'));
+app.set('view engine', 'hbs');
+app.engine('hbs', hbs.express4({
+	defaultLayout: __dirname + '/htdocs/index.hbs',
+	partialsDir: __dirname + '/htdocs/views/partials',
+	layoutsDir: __dirname + '/htdocs/views/layouts'
+}));
+
+
+
+hbs.registerHelper('ifCond', function (value1, value2, options) {
+	if (value1 === value2) {
+		return options.fn(this);
+	}
+	return options.inverse(this);
+
+});
+
+hbs.registerHelper('slice', function (array, start, end, options) {
+	var arraySlice = array.slice(start, end);
+	return arraySlice;
+});
+
+app.set('views', path.join(__dirname, '/htdocs/views'));
+app.use(express.static(path.join(__dirname, '/htdocs')));
+
+app.get('/', function (req, res) {
+	request('http://localhost:3000/data.json', function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			var data = JSON.parse(body);
+			res.render('home', data);
+		}
+	});
+});
 
 app.listen(3000, function () {
 	console.log('listen on http://localhost:3000');
